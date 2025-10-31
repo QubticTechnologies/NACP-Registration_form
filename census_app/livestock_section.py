3936a14904d74ce33b185bb532b1bc294bb9a9e9
QubticTechnologies <167151380+QubticTechnologies@users.noreply.github.com>
2025-10-31 07:21:20 -0400

    Delete census_app/livestock_section.py

--- census_app/livestock_section.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def livestock_section(holder_id):
-    st.subheader("Livestock Section")
-    st.info("This section is under construction.")
-    # You can add your livestock form here
